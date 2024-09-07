1e4c98b517ccd194c694ee6f2f2fa5f6fd495115
junaid alam <77788942+Junaid-12@users.noreply.github.com>
2024-09-08 04:11:21 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,50 @@
+//tag line Animation
+
+var typed = new Typed('#Element', {
+   strings: ['Welocme To My Portfolio', '&amp; Thank You!!!'],
+   typeSpeed: 50,
+   
+ });
+
+ 
+
+ //git hub buttion
+   function githupageopen(){
+    window.open('https://github.com/Junaid-12');
+
+  };
+
+  //download file
+ 
+
+//   document.getElementById("#gitbtn").addEventListener("click", function() {
+    
+//     fetch('D:\JavaScripte\protfoli\Mohdjunaidalam.pdf')
+    
+//         .then(response => response.blob())
+//         .then(blob => {
+//           console.log(blob);
+//             const url = window.URL.createObjectURL(new Blob([blob]));
+//             const a = document.createElement('a');
+//             a.href = url;
+//             a.download = 'file.pdf';
+//             document.body.appendChild(a);
+//             a.click();
+//             window.URL.revokeObjectURL(url);
+//         });
+// });
+
+// function downloadFile() {
+//   fetch('file:///D:/JavaScripte/protfoli/Mohdjunaidalam.pdf')
+//       .then(response => response.blob())
+//       .then(blob => {
+//           const url = window.URL.createObjectURL(new Blob([blob]));
+//           const a = document.createElement('a');
+//           a.href = url;
+//           a.download = 'file.pdf';
+//           document.body.appendChild(a);
+//           a.click();
+//           window.URL.revokeObjectURL(url);
+//       });
+// };
+ 
